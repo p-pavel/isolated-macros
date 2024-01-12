fdab59d69061baf049ef398a60b7a3aca899d124
Pavel Perikov <perikov@gmail.com>
2024-01-12 13:29:13 +0400

    Fixed dealiasing

--- modules/macros/src/main/scala/SomeTrait.scala
+++ modules/macros/src/main/scala/SomeTrait.scala
@@ -8,9 +8,18 @@ object SomeTrait {
   def impl[T[_]](c: Context)(implicit tt: c.WeakTypeTag[T[?]]) = {
     import c.universe._
     val tp = tt.tpe
-    c.info(c.enclosingPosition, s"$tt, $tp, ${showRaw(tp)}", force=true)
+    c.info(c.enclosingPosition, s"$tt, $tp, ${showRaw(tp)}", force = true)
     tp match {
-      case PolyType(_, TypeRef(_, sym, _)) if sym.fullName == "scala.Option" => c.info(c.enclosingPosition, "Got Option", true)
+      case poly @ PolyType(arg, tr @ TypeRef(_, sym, _)) =>
+        if (sym.fullName == "scala.Option")
+          c.info(c.enclosingPosition, "Got Option", true)
+        else {
+          c.info(c.enclosingPosition, s"Wrong type: $tr", true)
+          val dealised = tr.dealias
+          c.info(c.enclosingPosition, s"Dealised: ${showRaw((arg,dealised))}", true)
+          if (dealised.typeSymbol.fullName != "scala.Option")
+            c.abort(c.enclosingPosition, "Bad type structure")
+        }
       case _ => c.abort(c.enclosingPosition, "Bad type structure")
     }
 
